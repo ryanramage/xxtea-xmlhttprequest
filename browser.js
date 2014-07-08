643038400331cb946e6ca494529d11c787e8da98
Ryan Ramage <ryan.ramage@gmail.com>
2014-07-08 17:56:17 -0600

    trying to play with a fallback method on older browsers

--- browser.js
+++ browser.js
@@ -1,9 +1,42 @@
 var xxtea = require('xxtea-stream'),
+    streamifier = require('streamifier'),
     concat = require('concat-stream'),
     createReadStream = require('filereader-stream'),
     bops = require('bops');
 
-module.exports = function(url, pass, as_string, cb){
+module.exports = function(url, pass, as_string, cb) {
+  asBlob(url, pass, as_string, function(err, contents){
+    fallback(url, pass, as_string, cb);
+  });
+  
+
+}
+
+function fallback(url, pass, as_string, cb) {
+  console.log('fallback')
+  var oReq = new XMLHttpRequest();
+  oReq.open("GET", url, true);
+  oReq.overrideMimeType('text\/plain; charset=x-user-defined');
+  oReq.onreadystatechange = function() {
+    console.log('onreadystatechange');
+    if (oReq.readyState === 4) {
+      console.log(typeof oReq.responseText)
+      console.log('is binary', bops.is(oReq.responseText))
+      console.log(oReq.responseText);
+      console.log(bops.from(oReq.responseText, 'base64'))
+      streamifier.createReadStream(oReq.responseText, {})
+        .pipe(new xxtea.Decrypt(bops.from(pass, 'base64')))
+        .pipe(concat(function(contents) {
+          console.log('fallback', contents);
+          if (as_string) return cb(null, bops.to(contents));
+          else return cb(null, contents);
+        }))
+    }
+  }
+  oReq.send(null);
+}
+
+function asBlob(url, pass, as_string, cb) {
 
   // handle a call signature of function(url, pass, cb)
   if (typeof as_string === 'function') {
@@ -19,6 +52,7 @@ module.exports = function(url, pass, as_string, cb){
     createReadStream(oReq.response)
       .pipe(new xxtea.Decrypt(bops.from(pass, 'base64')))
       .pipe(concat(function(contents) {
+        console.log('blob', contents);
         if (as_string) return cb(null, bops.to(contents));
         else return cb(null, contents);
       }))
---
+++ tests/launcher.js
@@ -0,0 +1,6 @@
+console.log('start of the stuff');
+try {
+require('../browser')('out.text.tea', 'ODMzOWQ5M2pkb29lMmR3ZA==', true, function(err, data){
+    //alert(data);  
+})  
+} catch(e){console.log(e)}
